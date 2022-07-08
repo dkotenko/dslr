05262cded25bacaef30108d526c8a7cbc344a59e
Your Name <you@example.com>
2022-07-08 07:04:03 +0300

    venv added

--- describe.py
+++ describe.py
@@ -1,13 +1,33 @@
 import csv
-import os
+import sys
+import pandas as pd
+import numpy as np
 
-def describe(name):
-    with open('eggs.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        ...
-        print(', '.join(row))
+
+def describe():
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+    else:
+        print('Provide filepath as ./describe \'filepath\' or as argument to function describe(\'filepath\')')
+        sys.exit(0)
+
+    df = pd.read_csv(filepath,
+                     delimiter=',',
+                     index_col=0)
+
+    only_num = df.select_dtypes('number')
+    print(df.select_dtypes('number'))
+    print(list(only_num.columns))
+    exit(0)
+
+    with open(filepath, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            for cell in row:
+                if not cell.isnumeric():
+                    break
+            print(', '.join(row))
 
 
 if __name__ == '__main__':
-    describe('PyCharm')
+    describe()
